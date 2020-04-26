f7510e33e371988e1640a7a7b968a1e5103a7c0d
RyosukeOK <tanukiokpro@gmail.com>
2020-04-26 22:30:57 +0900

    👍 Functional improvement
    Add code for customLanguagesList.
    
    # ==== Emojis ====
    # 🐛 Fix a bug :bug: バグ修正
    # 👍 Functional improvement :+1: 機能改善
    # ✨ Introduce new features :sparkles: 部分的な機能追加
    # 🎉 Initial commit :tada: 盛大に祝うべき大きな機能追加
    # 🍱 Add or update assets :bento: アセットの追加または更新
    # 🚿 Unnecessary functions / Remove obsolete functions :shower: 不要な機能・使われなくなった機能の削除
    # 💚 Fix CI Build :green_heart: テストやCIの修正・改善
    # 👕 Fix Lint errors and modify code style :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀 Deploy stuff :rocket: パフォーマンス改善
    # 🆙 Updates such as dependency packages :up: 依存パッケージなどのアップデート
    # 🔒 Fix security issues :lock: 新機能の公開範囲の制限
    # 👮 Security related improvements :cop: セキュリティ関連の改善
    # ♻️ Refactor :recycle: リファクタリング
    # 🎨 Improve structure / format of the code :art: コードの構造/形式の改善
    # ⚡️ Improve performance :zap: パフォーマンスの向上
    # 🔥 Remove code or files :fire: コードまたはファイルの削除
    # 🚑 Critical hotfix :ambulance: 重要な修正プログラム
    # 📝 Write docs :memo: ドキュメントの作成
    # 💄 Update the UI and style files :lipstick: UIファイルとスタイルファイルの更新
    # ✅ Add tests :white_check_mark: テストの追加
    # 🚧 Work in progress :construction: 進行中の作業
    # 👷 Add CI build system :construction_worker: CIビルドシステムの追加
    # ✏️ Fix typos :pencil2: タイプミスの修正
    # ⏪ Revert changes :rewind: 変更を元に戻す
    # 🔀 Merge branches :twisted_rightwards_arrows: ブランチのマージ
    # 📦 Update compiled files or packages :package: コンパイル済みファイルまたはパッケージの更新
    # 👽 Update code due to external API changes :alien: 外部APIの変更によるコードの更新
    # 🚚 Move or rename files :truck: ファイルの移動または名前の変更
    # 📄 Add or update a license :page_facing_up: ライセンスの追加または更新
    # 💥 Introduce breaking changes :boom: 急激な変化を紹介する
    # 👌 Update code due to code review changes :ok_hand: コードレビューの変更によるコードの更新
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,12 +1,14 @@
 import 'package:language_pickers/languages.dart';
 
+import '../languages.dart';
+
 class LanguagePickerUtils {
-  static Language getLanguageByIsoCode(String isoCode) {
-    final _languages = defaultLanguagesList.map((item) => Language.fromMap(item)).toList();
+  static Language getLanguageByIsoCode(String isoCode, List<Map<String,String>>languagesList) {
+    final _languages = languagesList.map((item) => Language.fromMap(item)).toList();
     try {
       return _languages
           .where((language) =>
-              language.isoCode.toLowerCase() == isoCode.toLowerCase())
+      language.isoCode.toLowerCase() == isoCode.toLowerCase())
           .toList()[0];
     } catch (error) {
       throw Exception("The initialValue provided is not a supported iso code!");
@@ -17,7 +19,7 @@ class LanguagePickerUtils {
     return "assets/${isoCode.toLowerCase()}.png";
   }
 
-  /* static Widget getDefaultFlagImage(Language language) {
+/* static Widget getDefaultFlagImage(Language language) {
     return Image.asset(
       LanguagePickerUtils.getFlagImageAssetPath(language.isoCode),
       height: 20.0,
